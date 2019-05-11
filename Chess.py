6fafd5447bfeb86d5ae310f22a40dfb2bff02419
JangJang3 <50151199+JangJang3@users.noreply.github.com>
2019-05-11 21:41:01 +0200

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -30,21 +30,23 @@ class Chess(QWidget):
     def drawBlocks(self, qp):
 
         step = 125
+        colWhite = QColor(255, 255, 255)
+        colBlack = QColor(0, 0, 0)
 
         #each step draws a 2x2 block consisting of 4 "step"-sized blocks
         for X in range(0, 1000, 2*step):
             for Y in range(0, 1000, 2*step):
                 #White top left
-                qp.setBrush(QColor(255, 255, 255))
+                qp.setBrush(colWhite)
                 qp.drawRect(X , Y, step, step)
                 #Black top right
-                qp.setBrush(QColor(0, 0, 0))
+                qp.setBrush(colBlack)
                 qp.drawRect(X+step, Y, step, step)
                 #Black bottom left
-                qp.setBrush(QColor(0, 0, 0))
+                qp.setBrush(colBlack)
                 qp.drawRect(X , Y+step, step, step)
                 #White bottom right
-                qp.setBrush(QColor(255, 255, 255))
+                qp.setBrush(colWhite)
                 qp.drawRect(X+step, Y+step, step, step)      
         
 """
