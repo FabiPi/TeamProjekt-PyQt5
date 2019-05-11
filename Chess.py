2f5f0ec8d62eb2d3fbd5a2bcfcdc210969106ec1
B-Dome <50382067+B-Dome@users.noreply.github.com>
2019-05-11 16:11:42 +0200

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -103,14 +103,19 @@ von Dominik
         col.setNamedColor('#d4d4d4')
         qp.setPen(col)
 
+        #each step draws a 2x2 block consisting of 4 "step"-sized blocks
         for X in range(0, 1000, 2*step):
             for Y in range(0, 1000, 2*step):
+                #White top left
                 qp.setBrush(QColor(255, 255, 255))
                 qp.drawRect(X , Y, step, step)
+                #Black top right
                 qp.setBrush(QColor(0, 0, 0))
                 qp.drawRect(X+step, Y, step, step)
+                #Black bottom left
                 qp.setBrush(QColor(0, 0, 0))
                 qp.drawRect(X , Y+step, step, step)
+                #White bottom right
                 qp.setBrush(QColor(255, 255, 255))
                 qp.drawRect(X+step, Y+step, step, step)
 """
