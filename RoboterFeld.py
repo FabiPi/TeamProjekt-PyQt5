a709c02350ca80cb7b780e4212aedc0ed9370b1b
JangJang3 <50151199+JangJang3@users.noreply.github.com>
2019-05-17 21:12:19 +0200

    Update RoboterFeld.py

--- RoboterFeld.py
+++ RoboterFeld.py
@@ -2,14 +2,17 @@
 Roboter Feld
 von B-Dome, JangJang3, FabiPi
 """
-
-from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget, QMessageBox
+from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
 from PyQt5.QtGui import QPainter, QColor, QBrush
 from PyQt5.QtCore import Qt, QBasicTimer
 import sys
-
+import time
 
 class SpielFeld(QWidget):
+
+
+    PlayFieldAR = [[0 for x in range(100)] for y in range(100)]
+
     Speed = 500
 
     def __init__(self):
@@ -26,8 +29,6 @@ class SpielFeld(QWidget):
         self.timer.start(SpielFeld.Speed, self)
         self.show()
 
-
-
     def center(self):
         '''centers the window on the screen'''
 
@@ -39,13 +40,22 @@ class SpielFeld(QWidget):
     def timerEvent(self, event):
         screen = QDesktopWidget().screenGeometry()
 
-        if (BaseRobot.xPosition == (screen.width() - (BaseRobot.radius +10))) or (BaseRobot.yPosition == (screen.height() - (BaseRobot.radius + 10))):
+        if (BaseRobot.xPosition == (screen.width() - (BaseRobot.radius + 10))) or (
+                BaseRobot.yPosition == (screen.height() - (BaseRobot.radius + 10))):
             self.timer.stop()
         else:
             BaseRobot.xPosition += 10
 
+    def center(self):
+        '''centers the window on the screen'''
 
-    def paintEvent(self, qp):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2,
+                  (screen.height() - size.height()) / 2)
+
+
+    def paintEvent(self, qp ):
 
         qp = QPainter()
         qp.begin(self)
@@ -56,49 +66,36 @@ class SpielFeld(QWidget):
     def drawField(self, qp):
 
         #Array construction
-        width = 100
-        height = 100
-        PlayFieldAR = [[0 for x in range(width)] for y in range(height)]
 
         #set Walls, set array value to 1 to place Wall
 
         #set Wall around the edges
         for x in range(0,100,1):
-            PlayFieldAR[x][0]= 1
-            PlayFieldAR[x][99]= 1
+            SpielFeld.PlayFieldAR[x][0]= 1
+            SpielFeld.PlayFieldAR[x][99]= 1
         for y in range(1,99,1):
-            PlayFieldAR[0][y]= 1
-            PlayFieldAR[99][y]= 1
+            SpielFeld.PlayFieldAR[0][y]= 1
+            SpielFeld.PlayFieldAR[99][y]= 1
 
         #set some Obstacle
         for i in range(0, 25, 1):
-            PlayFieldAR[70][i+45] = 1
+            SpielFeld.PlayFieldAR[70][i+45] = 1
 
         for i in range(0, 40, 1):
-            PlayFieldAR[i+10][40] = 1
+            SpielFeld.PlayFieldAR[i+10][40] = 1
         for i in range(0, 50, 1):
-            PlayFieldAR[i+30][70] = 1
+            SpielFeld.PlayFieldAR[i+30][70] = 1
 
         for i in range(0, 30, 1):
-            PlayFieldAR[i+25][20] = 1
+            SpielFeld.PlayFieldAR[i+25][20] = 1
 
         for i in range(0, 10, 1):
-            PlayFieldAR[10][i+50] = 1
-
-        """
-        Randomisierte Verteilung von field oder wall
-        for i in range (0,100,1):
-            for j in range(0,100,1):
-                if (PlayFieldAR[i-1][i-1] != 1):
-                    PlayFieldAR[i][j] = random.randint(0,1)
-                else:
-                    PlayFieldAR[i][j] = 0
-        """
+            SpielFeld.PlayFieldAR[10][i+50] = 1
 
         #Draw the PlayField
         for i in range(0, 100, 1):
             for j in range(0, 100, 1):
-                    if PlayFieldAR[i][j]==1:
+                    if SpielFeld.PlayFieldAR[i][j]==1:
                         qp.setBrush(QColor(0, 0, 0))
                         qp.drawRect(i*10, j*10, 10, 10)
                     else:
@@ -114,58 +111,41 @@ class SpielFeld(QWidget):
 
     def keyPressEvent(self, event):
         '''process key press'''
+        RobotX = int(round(BaseRobot.xPosition/10))
+        RobotY = int(round(BaseRobot.yPosition/10))
+
         key = event.key()
 
         if key == Qt.Key_W:
-            BaseRobot.yPosition -= 10
+            if SpielFeld.PlayFieldAR[RobotX][RobotY-1] == SpielFeld.PlayFieldAR[RobotX+1][RobotY-1] == SpielFeld.PlayFieldAR[RobotX+2][RobotY-1] == 0:
+                BaseRobot.yPosition -= 10
             return
 
         elif key == Qt.Key_S:
-            BaseRobot.yPosition += 10
+            if SpielFeld.PlayFieldAR[RobotX][RobotY+3] == SpielFeld.PlayFieldAR[RobotX+1][RobotY+3] == SpielFeld.PlayFieldAR[RobotX+2][RobotY+3] == 0:
+                BaseRobot.yPosition += 10
             return
 
         elif key == Qt.Key_A:
-            BaseRobot.xPosition -= 10
+            if SpielFeld.PlayFieldAR[RobotX-1][RobotY] == SpielFeld.PlayFieldAR[RobotX-1][RobotY+1] == SpielFeld.PlayFieldAR[RobotX-1][RobotY+2] == 0:
+                BaseRobot.xPosition -= 10
             return
 
         elif key == Qt.Key_D:
-            BaseRobot.xPosition += 10
+            if SpielFeld.PlayFieldAR[RobotX+3][RobotY] == SpielFeld.PlayFieldAR[RobotX+3][RobotY+1] == SpielFeld.PlayFieldAR[RobotX+3][RobotY+2] == 0:
+                BaseRobot.xPosition += 10
             return
-
-
-        """
-        Trivial Step idea but crash
-        
-        while True:
-            br.drawEllipse(BaseRobot.xPosition, BaseRobot.yPosition, 2* BaseRobot.radius, 2*BaseRobot.radius)
-            BaseRobot.xPosition = BaseRobot.xPosition +1
-            time.sleep(1)
-        """
-
-
-
 
 
 class BaseRobot (QWidget):
     xPosition = 20
     yPosition = 30
-    radius = 5
+    radius = 15
     alpha = 45
-
-"""
-    def paintEvent(self, e):
-        br = QPainter()
-        br.begin(self)
-        self.drawField(e)
-        br.end()
-    def drawField(self, br):
-        br.setBrush(QColor(255, 0, 0))
-        br.drawEllipse(50, 50, 50 ,50)
-"""
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = SpielFeld()
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
