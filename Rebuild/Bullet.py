7ed96843403da4ea181153b7c9d7469566d881e8
B-Dome <50382067+B-Dome@users.noreply.github.com>
2019-07-03 04:47:20 +0200

    Update Bullet.py

--- Rebuild/Bullet.py
+++ Rebuild/Bullet.py
@@ -1,3 +1,19 @@
+"""
+Roboter Feld
+von B-Dome, JangJang3, FabiPi
+"""
+
+
+from PyQt5.QtGui import QPainter, QColor, QBrush, QVector2D, QPixmap, QPainterPath
+import math
+import Server
+import Robots
+
+#####  BULLET   ######
+
+Bullet_Size = 10
+Bullet_Speed =5
+
 class Bullet(object):
     def __init__(self, position, velocity):
         self.position = position
