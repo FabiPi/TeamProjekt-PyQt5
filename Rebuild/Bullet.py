78a80e378941411802f87b147d6a573f110bc2fd
JangJang3 <50151199+JangJang3@users.noreply.github.com>
2019-07-03 04:19:12 +0200

    bullet hit

--- Rebuild/Bullet.py
+++ Rebuild/Bullet.py
@@ -1,17 +1,3 @@
-"""
-Roboter Feld
-von B-Dome, JangJang3, FabiPi
-"""
-
-
-from PyQt5.QtGui import QPainter, QColor, QBrush, QVector2D, QPixmap, QPainterPath
-import math
-import Server
-import Robots
-
-Bullet_Size = 10
-Bullet_Speed =5
-
 class Bullet(object):
     def __init__(self, position, velocity):
         self.position = position
@@ -24,6 +10,17 @@ class Bullet(object):
 
     def moveBullet(self):
         self.position.__iadd__(self.velocity)
+
+    def bulletShape(self):
+        shape = QPainterPath()
+        shape.addEllipse(self.position.x() - (0.5 * Bullet_Size) , self.position.y() - (0.5 * Bullet_Size), Bullet_Size, Bullet_Size)
+        return shape
+
+    def one_hit(self, robo):
+        if self.bulletShape().intersects(robo.roboShape()):
+            return True
+        else: pass
+
         
         
         
